a9c29b5a65b6373c5d0fd1433098aaf47edf90fe
nialdaly <nial@cofide.io>
2025-05-14 09:36:00 +0100

    InsecureSkipVerifyEnvVar const added

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -3,5 +3,6 @@ package consts
 const (
 	APITokenEnvVarKey        = "COFIDE_API_TOKEN"
 	ConnectURLEnvVarKey      = "COFIDE_CONNECT_URL"
+	InsecureSkipVerifyEnvVar = "COFIDE_INSECURE_SKIP_VERIFY"
 	ServerAuthoritySubdomain = "connect"
 )
