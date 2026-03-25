070d95c07fde76aecf0741223c4363212da96a50
Mark Goddard <mark@cofide.io>
2026-03-25 11:37:20 +0000

    chore: enrich examples

--- examples/resources/cofide_connect_ap_binding/resource.tf
+++ examples/resources/cofide_connect_ap_binding/resource.tf
@@ -3,3 +3,15 @@ resource "cofide_connect_ap_binding" "example_ap_binding" {
   trust_zone_id = "example-tz-id"
   policy_id     = "example-ap-id"
 }
+
+resource "cofide_connect_ap_binding" "example_ap_binding_with_federations" {
+  org_id        = "example-org-id"
+  trust_zone_id = "example-tz-id"
+  policy_id     = "example-ap-id"
+
+  federations = [
+    {
+      trust_zone_id = "example-remote-tz-id"
+    }
+  ]
+}
