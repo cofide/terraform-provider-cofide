1d71e76487e09e0e7955ddd9eff62a4175a83464
Alec Wilson <alec-w@users.noreply.github.com>
2026-09-17 14:23:09 +0100

    Add v1alpha1 suffix to role _binding resource (#188)
    Unversioned resource is kept supported as deprecated alias
    of the versioned one.

--- examples/resources/cofide_connect_role_binding_v1alpha1/user_binding/outputs.tf
+++ examples/resources/cofide_connect_role_binding_v1alpha1/user_binding/outputs.tf
@@ -1,4 +1,4 @@
 output "role_binding_id" {
   description = "The ID of the role binding."
-  value       = cofide_connect_role_binding.example.id
+  value       = cofide_connect_role_binding_v1alpha1.example.id
 }
